147b997cc6614d7ded129454a7bdc96c435c8485
Westporch <westporch@gmail.com>
2017-07-16 08:45:00 +0900

    post_list 함수 추가

--- dev/askdjango/blog/views.py
+++ dev/askdjango/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+'''
+render를 import하지 않고 3~4줄로 작성하는 것은 오래된 코드 방식이다.
+간단히 from django.shortcuts import render 1줄로 작성한다.
+'''
 
-# Create your views here.
+def post_list(request):
+	return render(request, 'blog/post_list.html')

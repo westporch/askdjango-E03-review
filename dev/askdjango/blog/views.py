bb41d533124bb275ec33a46d157a4a10d7f12b08
Westporch <westporch@gmail.com>
2017-07-16 10:17:56 +0900

    인자가 1개인 mysum 함수 추가

--- dev/askdjango/blog/views.py
+++ dev/askdjango/blog/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 ''' from django.shortcuts import render에 대한 설명
@@ -9,3 +10,10 @@ render를 import하지 않고 3~4줄로 작성하는 것은 오래된 코드 방
 def post_list(request):
 	#print(request.META['REMOTE_ADDR']) # 웹으로 django 접속시 remote host의 ip를 runserver 콘솔에 출력한다.
 	return render(request, 'blog/post_list.html')
+
+def mysum(request, x):
+	'''리턴값: URL에 입력한 값을 리턴하여 웹페이지에 출력한다.
+	사용법(예): 웹브라우저의 주소 창에 아래 주소를 입력한다. (999가 출력된다.)
+				http://192.168.0.17:8080/blog/sum/999/
+	'''
+	return HttpResponse(int(x))

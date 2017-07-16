950b133a30db2a5b58d0bcc915cf143d6403d826
Westporch <westporch@gmail.com>
2017-07-16 20:10:03 +0900

    1개의 뷰(mysum)에 3개의 URL을 연결함

--- dev/askdjango/blog/views.py
+++ dev/askdjango/blog/views.py
@@ -11,21 +11,21 @@ def post_list(request):
 	#print(request.META['REMOTE_ADDR']) # 웹으로 django 접속시 remote host의 ip를 runserver 콘솔에 출력한다.
 	return render(request, 'blog/post_list.html')
 
-def mysum(request, x):
-	''' 인자: 정수 1개
-    리턴값: URL에 입력한 값(정수 1개)을 리턴하여 웹페이지에 출력한다.
-	사용법(예): 웹브라우저의 주소 창에 아래 주소를 입력한다. (999가 출력된다.)
-				http://192.168.0.17:8080/blog/sum/999/
-	'''
-	return HttpResponse(int(x))
+#def mysum(request, x):
+#	''' 인자: 정수 1개
+#    리턴값: URL에 입력한 값(정수 1개)을 리턴하여 웹페이지에 출력한다.
+#	사용법(예): 웹브라우저의 주소 창에 아래 주소를 입력한다. (999가 출력된다.)
+#				http://192.168.0.17:8080/blog/sum/999/
+#	'''
+#	return HttpResponse(int(x))
 
-def mysum(request, x, y):
-	''' 인자: 정수 2개
-    리턴값: URL에 입력한 값(정수 2개)을 더한 뒤 리턴하여 웹페이지에 출력한다.
-	사용법(예): 웹브라우저의 주소 창에 아래 주소를 입력한다. (1000이 출력된다.)
-				http://192.168.0.17:8080/blog/sum/999/1
-	'''
-	return HttpResponse(int(x) + int(y))
+#def mysum(request, x, y):
+#	''' 인자: 정수 2개
+#    리턴값: URL에 입력한 값(정수 2개)을 더한 뒤 리턴하여 웹페이지에 출력한다.
+#	사용법(예): 웹브라우저의 주소 창에 아래 주소를 입력한다. (1000이 출력된다.)
+#				http://192.168.0.17:8080/blog/sum/999/1
+#	'''
+#	return HttpResponse(int(x) + int(y))
 
 def mysum(request, x, y=0, z=0):
 	''' 인자: 정수 3개
